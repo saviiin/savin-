6d5d6c0ba00a1607576f8f894b864669598bd672
saviiin <119603534+saviiin@users.noreply.github.com>
2022-12-01 21:35:03 +0545

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,39 @@
+/* Write a program that takes name and age of a 5 persons as an input and
+gives the degree of membership of the person as its output according to
+following membership functions. */
+
+#include<stdio.h>
+
+struct Person{
+	char name[50];
+	int age;
+};
+
+int main(){
+	int i;
+	struct Person p[5];
+	float b;
+	
+	for(i=0;i<5;i++){
+		printf("Enter the name and age of %d person: \n",i+1);
+		scanf("%s",&p[i].name);
+	
+		scanf("%d",&p[i].age);
+		
+		
+	}
+	for(i=0;i<5;i++){
+		if(p[i].age<=15){
+			printf("The degree of membership of %s is 1.\n",p[i].name);
+		}
+		else if(p[i].age>15 && p[i].age<=35){
+			b = (35-p[i].age)*0.05;
+			printf("The degree of membership of %s is %0.2f.\n",p[i].name,b);
+		}
+		else{
+			printf("The degree of membership of %s is 0.\n",p[i].name);
+		}
+	}
+	
+	return 0;
+}
